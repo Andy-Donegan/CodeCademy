6b864604d221489421e5b939f18f8161ab7facb5
Andy Donegan <andy_donegan@hotmail.co.uk>
2022-01-19 17:56:15 +0000

    New module

---
+++ Js/Learn Intermediate Js/Classes/Constructor/main.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+class Surgeon {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+// create new Surgeon object and log output to console for testing.
+const timmySurgeon = new Surgeon("Timmy", "Urinals");
+
+console.log(timmySurgeon);
