e317b0480fc8e28991dc15b301c338e25e26afd4
Andy Donegan <andy_donegan@hotmail.co.uk>
2022-01-20 11:48:06 +0000

    Inheritance 4 completed

--- Js/Learn Intermediate Js/Classes/Inheritance4/main.js
+++ Js/Learn Intermediate Js/Classes/Inheritance4/main.js
@@ -1,1 +1,33 @@
 // JavaScript source code
+
+class HospitalEmployee {
+    constructor(name) {
+        this._name = name;
+        this._remainingVacationDays = 20;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get remainingVacationDays() {
+        return this._remainingVacationDays;
+    }
+
+    takeVacationDays(daysOff) {
+        this._remainingVacationDays -= daysOff;
+    }
+}
+
+class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+        super(name);
+        this._certifications = certifications;
+    }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
+// the new Nurse class extends HospitalEmployee allowing access to all of the functionality of the Parent class get/set Methods etc.
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
