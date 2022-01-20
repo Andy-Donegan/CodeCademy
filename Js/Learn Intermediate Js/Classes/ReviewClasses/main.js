cdb01b95e0a67531970bbf31018f7fd56bc99746
Andy Donegan <andy_donegan@hotmail.co.uk>
2022-01-20 14:18:54 +0000

    Completed Review Classes

--- Js/Learn Intermediate Js/Classes/ReviewClasses/main.js
+++ Js/Learn Intermediate Js/Classes/ReviewClasses/main.js
@@ -1,1 +1,83 @@
 // JavaScript source code
+
+// Amended the static method to take an obj and return the name with a password suggestion (basic)
+class HospitalEmployee {
+    constructor(name) {
+        this._name = name;
+        this._remainingVacationDays = 20;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get remainingVacationDays() {
+        return this._remainingVacationDays;
+    }
+
+    takeVacationDays(daysOff) {
+        this._remainingVacationDays -= daysOff;
+    }
+
+    static generatePassword(obj) {
+        return (`${obj.name} should use: ` + Math.floor(Math.random() * 9999 + 1));
+    }
+}
+
+class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+        super(name);
+        this._certifications = certifications;
+    }
+
+    get certifications() {
+        return this._certifications;
+    }
+
+    addCertification(newCertification) {
+        this.certifications.push(newCertification);
+    }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+// Example Doctor class with inherits from Hospital Employee also adding get for insurance
+class Doctor extends HospitalEmployee {
+    constructor(name, insurance) {
+        super(name);
+        this._insurance = insurance;
+    }
+
+    get insurance() {
+        return this._insurance;
+    }
+}
+
+// Example Janitor Staff with department and ability to add new departments.
+class Janitor extends HospitalEmployee {
+    constructor(name, department) {
+        super(name);
+        this._department = department;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    addDepartment(newDepartment) {
+        this._department.push(newDepartment);
+    }
+}
+
+const janitorSmith = new Janitor('John Smith', ['Pediatrics']);
+console.log(janitorSmith.department);
+
+janitorSmith.addDepartment('Trauma');
+console.log(janitorSmith.department);
+// Call static method in HospitalEmployee
+console.log(HospitalEmployee.generatePassword(janitorSmith));
+
--- Js/Learn Intermediate Js/Classes/ReviewClasses/readme.md
+++ Js/Learn Intermediate Js/Classes/ReviewClasses/readme.md
@@ -2,3 +2,21 @@
 
 - [Index](https://github.com/Andy-Donegan/CodeCademy/tree/main/Js/Learn%20Intermediate%20Js/Classes).
 - [Previous Static Methods](https://github.com/Andy-Donegan/CodeCademy/tree/main/Js/Learn%20Intermediate%20Js/Classes/StaticMethods).
+
+Way to go! Let’s review what you learned.
+
+- Classes are templates for objects.
+- Javascript calls a constructor method when we create a new instance of a class.
+- Inheritance is when we create a parent class with properties and methods that we can extend to child classes.
+- We use the extends keyword to create a subclass.
+- The super keyword calls the constructor() of a parent class.
+- Static methods are called on the class, but not on instances of the class.
+
+In completing this lesson, you’ve taken one step closer to writing efficient, production-level JavaScript. Good luck as you continue to develop your skills and move into intermediate-level concepts.
+
+Instructions
+If you want extra practice with inheritance, take some time to create a Doctor class that inherits from HospitalEmployee. The properties and methods for the Doctor class are listed below:
+
+Doctor
+- properties: _name, _remainingVacationDays (set to 20 inside constructor()), _insurance
+- methods: .takeVacationDays()
