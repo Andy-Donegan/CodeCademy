161dd4574890184674c94ca782988115d88bc944
Andy Donegan <andy_donegan@hotmail.co.uk>
2022-01-22 10:34:24 +0000

    Handling Independent Promises complete

---
+++ Js/Learn Intermediate Js/AsyncAwait/HandlingIndependentPromises/library.js
@@ -0,0 +1,34 @@
+// JavaScript source code
+let cookBeans = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('beans');
+        }, 1000);
+    });
+}
+
+let steamBroccoli = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('broccoli');
+        }, 1000);
+    });
+}
+
+let cookRice = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('rice');
+        }, 1000);
+    });
+}
+
+let bakeChicken = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('chicken');
+        }, 1000);
+    });
+}
+
+module.exports = { cookBeans, steamBroccoli, cookRice, bakeChicken };
